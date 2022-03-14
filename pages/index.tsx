adffd21bd7cdcdd76d3a8d9e626b341255c771f4
Gustavo Monteiro <gustavosmonteiro@gmail.com>
2022-03-14 18:24:10 +0000

    feat: improve pagination with Intersection Observer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useCallback, useEffect, useState } from 'react';
 
 import PokemonList from '@/components/pokemon-list';
+
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 
 import { usePokemonListQuery } from '@/graphql-generated';
 
@@ -11,6 +14,27 @@ const Home: NextPage = () => {
 	const { data, loading, error, fetchMore } = usePokemonListQuery({
 		variables: { limit: 20 },
 	});
+
+	const [ref, setRef] = useState<Element | null>(null);
+	const entry = useIntersectionObserver(ref as Element, {});
+	const isVisible = !!entry?.isIntersecting;
+
+	const getRef = useCallback((node) => {
+		if (node !== null) {
+			setRef(node);
+		}
+	}, []);
+
+	useEffect(() => {
+		if (isVisible) {
+			fetchMore({
+				variables: {
+					offset: data?.pokemons.length,
+				},
+			});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isVisible]);
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error :(</p>;
@@ -29,18 +53,7 @@ const Home: NextPage = () => {
 			<main>
 				<StyledH1>Pokemons</StyledH1>
 				{data?.pokemons && <PokemonList pokemons={data?.pokemons} />}
-
-				<button
-					onClick={() =>
-						fetchMore({
-							variables: {
-								offset: data?.pokemons.length,
-							},
-						})
-					}
-				>
-					Load More
-				</button>
+				<span ref={getRef} />
 			</main>
 		</>
 	);
